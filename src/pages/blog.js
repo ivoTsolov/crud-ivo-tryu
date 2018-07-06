56499a478d09e625c8fee02f8483786bf8726244
alpox <elias.be@gmx.ch>
2018-07-07 01:07:10 +0200

    teaching

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,100 +1,159 @@
-  //basic stuff
-  import React, { Component } from 'react';
-  import {connect} from 'react-redux';
-  //action creators and ajax
-  import {update, create, getPosts, editPost, deletePost} from '../reducers/posts';
-
-  //ui elements
-  import {Row} from 'reactstrap';
-  
-
-  //components
-  class Blog extends Component {
-    constructor(props) {
-      super(props); 
-      this.state = {postToEdit: null};
-      this.editPostAndHide = this.editPostAndHide.bind(this);
-      this.saveEditedPost = this.saveEditedPost.bind(this);
-
-    
-    }
-
-  editPostAndHide(content){
-    this.props.editPost(content).then(()=>{
-      this.setState({postToEdit: null});
-    })
-    
-  }
-
-
-  saveEditedPost(params) {
-      this.setState({
-        postToEdit: {
-          ...this.state.postToEdit,
-          post: params
-        }
-      })
-  }
-  
-  
-
-
-
-    render() {
-      return (
-      
-        <Row>
-          <form onSubmit={(event)=>{
-            event.preventDefault();
-            this.props.create({ post: this.props.post })}}>
-              <h1>Blog List</h1>
-              <input type="text" name="post" value={this.props.post} onChange={(e)=>{this.props.update(e.target.name, e.target.value)}}/>
-              <button type="submit">POST</button>
-          </form>
-          <form onSubmit={(event)=>{
-            event.preventDefault();
-            this.props.getPosts();
-          }}>
-            <button type="submit">SHOW ALL POSTS</button>
-          </form>
-          
-            {this.props.posts.map((post, idx)=>{
-              return <p key={idx}>{post.post}<button onClick={()=>{this.setState({postToEdit: post})}}>Edit</button>
-              <button onClick={()=>{this.props.deletePost(post._id)}}>Delete</button>
-              </p>
-            })}
-          
-          { this.state.postToEdit &&
-          <div>
-            <textarea onChange={(event)=>{
-                this.saveEditedPost(event.target.value);
-            }}>
-              {this.state.postToEdit.post}
-            </textarea>
-            <button onClick={()=>{this.editPostAndHide(this.state.postToEdit)}}>SAVE</button>
-
-          </div>
-
-          }
-    
-        </Row>
-  
-      );
-    }
-  }
-
-  function mapStateToProps(state){
-    return{
-      post: state.posts.post,
-      posts: state.posts.posts
-    }
-  }
-  export default connect(mapStateToProps, { 
+//basic stuff
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+//action creators and ajax
+import {
     update,
     create,
     getPosts,
     editPost,
     deletePost
+} from '../reducers/posts';
 
+//ui elements
+import { Row } from 'reactstrap';
 
-  })(Blog);
+//components
+class Blog extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { postToEdit: null };
+        this.editPostAndHide = this.editPostAndHide.bind(this);
+        this.changePostProperty = this.changePostProperty.bind(this);
+    }
+
+    editPostAndHide() {
+        const {
+            postToEdit: { _id, title, body, imagePath }
+        } = this.state;
+
+        this.props.editPost(_id, title, body, imagePath).then(() => {
+            this.setState({ postToEdit: null });
+        });
+    }
+
+    changePostProperty(e) {
+        this.setState({
+            postToEdit: {
+                ...this.state.postToEdit,
+                [e.target.name]: e.target.value
+            }
+        });
+    }
+
+    render() {
+        return (
+            <Row>
+                <form
+                    onSubmit={event => {
+                        event.preventDefault();
+                        this.props.create(
+                            this.props.title,
+                            this.props.imagePath,
+                            this.props.body
+                        );
+                    }}
+                >
+                    <h1>Blog List</h1>
+                    <input
+                        type="text"
+                        name="title"
+                        value={this.props.title}
+                        onChange={e => {
+                            this.props.update(e.target.name, e.target.value);
+                        }}
+                    />
+                    <input
+                        type="text"
+                        name="imagePath"
+                        value={this.props.imagePath}
+                        onChange={e => {
+                            this.props.update(e.target.name, e.target.value);
+                        }}
+                    />
+                    <textarea
+                        type="text"
+                        name="body"
+                        onChange={e => {
+                            this.props.update(e.target.name, e.target.value);
+                        }}
+                    >
+                        {this.props.body}
+                    </textarea>
+                    <button type="submit">POST</button>
+                </form>
+                <form
+                    onSubmit={event => {
+                        event.preventDefault();
+                        this.props.getPosts();
+                    }}
+                >
+                    <button type="submit">SHOW ALL POSTS</button>
+                </form>
+
+                {this.props.posts.map((post, idx) => {
+                    return (
+                        <p key={idx}>
+                            {post.title}
+                            <button
+                                onClick={() => {
+                                    this.setState({ postToEdit: post });
+                                }}
+                            >
+                                Edit
+                            </button>
+                            <button
+                                onClick={() => {
+                                    this.props.deletePost(post._id);
+                                }}
+                            >
+                                Delete
+                            </button>
+                        </p>
+                    );
+                })}
+
+                {this.state.postToEdit && (
+                    <div>
+                        <input
+                            name="title"
+                            value={this.state.postToEdit.title}
+                            onChange={this.changePostProperty}
+                        />
+                        <input
+                            name="imagePath"
+                            value={this.state.postToEdit.imagePath}
+                            onChange={this.changePostProperty}
+                        />
+                        <textarea
+                            name="body"
+                            onChange={this.changePostProperty}
+                        >
+                            {this.state.postToEdit.body}
+                        </textarea>
+                        <button onClick={this.editPostAndHide}>SAVE</button>
+                    </div>
+                )}
+            </Row>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        body: state.posts.body,
+        title: state.posts.title,
+        imagePath: state.posts.imagePath,
+        posts: state.posts.posts
+    };
+}
+export default connect(
+    mapStateToProps,
+    {
+        update,
+        create,
+        getPosts,
+        editPost,
+        deletePost
+    }
+)(Blog);
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,217 +1,216 @@
-import {POST_CREATED, ACCOUNT_FAILED} from '../actions/actionTypes';
-import {POST_FAILED} from '../actions/actionTypes';
-import {UPDATE_A_POST} from '../actions/actionTypes';
-import {SET_POSTS} from '../actions/actionTypes';
-import {ACCOUNT_CREATED} from '../actions/actionTypes';
-import {LOGIN_SUCCESS} from '../actions/actionTypes';
-import {LOGIN_FAILED} from '../actions/actionTypes';
+import { POST_CREATED, ACCOUNT_FAILED } from '../actions/actionTypes';
+import { POST_FAILED } from '../actions/actionTypes';
+import { UPDATE_A_POST } from '../actions/actionTypes';
+import { SET_POSTS } from '../actions/actionTypes';
+import { ACCOUNT_CREATED } from '../actions/actionTypes';
+import { LOGIN_SUCCESS } from '../actions/actionTypes';
+import { LOGIN_FAILED } from '../actions/actionTypes';
 import axios from 'axios';
 
 // hash import
 import bcrypt from 'bcryptjs';
 //
 
-
 const saltEnv = process.env.REACT_APP_SECRET_CODE;
 
 const initialState = {
     post: '',
     postCreated: null,
-    posts : [
-
-    ],
+    posts: [],
     registered: false,
-    username: "",
-    password: "",
+    username: '',
+    password: '',
     logedIn: null
-}
+};
 
- export default function posts(state = initialState, action)  {
-      
-    switch(action.type){
-         case UPDATE_A_POST:
+export default function posts(state = initialState, action) {
+    switch (action.type) {
+        case UPDATE_A_POST:
             return {
                 ...state,
-                 [action.payload.key] : action.payload.value
-            }
-        case POST_CREATED: 
-            return {
-            ...state,
-            postCreated: true
-        }
-        case POST_FAILED: 
+                [action.payload.key]: action.payload.value
+            };
+        case POST_CREATED:
             return {
                 ...state,
-            postCreated: false
-
-            }
-        case ACCOUNT_CREATED: 
+                postCreated: true
+            };
+        case POST_FAILED:
+            return {
+                ...state,
+                postCreated: false
+            };
+        case ACCOUNT_CREATED:
             return {
                 ...state,
                 registered: true
-            }
-        case ACCOUNT_FAILED: 
+            };
+        case ACCOUNT_FAILED:
             return {
                 ...state,
                 registered: false
-            }
-        case LOGIN_SUCCESS: 
+            };
+        case LOGIN_SUCCESS:
             return {
                 ...state,
                 logedIn: true
-            }
+            };
         case LOGIN_FAILED:
             return {
                 ...state,
                 logedIn: false
-            }
-        case SET_POSTS: 
-            return { 
+            };
+        case SET_POSTS:
+            return {
                 ...state,
                 posts: action.payload
+            };
+        default:
+            return state;
+    }
+}
 
-            }
-        default: return state
-        }
- }
-
- export function update(key,value){ 
+export function update(key, value) {
     return {
         type: UPDATE_A_POST,
         payload: {
             key: key,
             value: value
         }
-    }
- }
+    };
+}
 //posts
- export function post_created() {
+export function post_created() {
     return {
         type: POST_CREATED
-    }
- }
- export function post_failed(){
-     return {
-         type: POST_FAILED
-     }
- }
+    };
+}
+export function post_failed() {
+    return {
+        type: POST_FAILED
+    };
+}
 
+// accounts
+export function account_created() {
+    return {
+        type: ACCOUNT_CREATED
+    };
+}
 
- // accounts
- export function account_created(){
-     return {
-         type: ACCOUNT_CREATED
-     }
- }
+export function account_failed() {
+    return {
+        type: ACCOUNT_FAILED
+    };
+}
 
- export function account_failed(){
-     return {
-         type: ACCOUNT_FAILED
-     }
- }
-
-export function login_success(){
+export function login_success() {
     return {
         type: LOGIN_SUCCESS
-    }
+    };
 }
 
-export function login_failed(){
+export function login_failed() {
     return {
         type: LOGIN_FAILED
-    }
+    };
 }
 
- //
-export function set_posts(posts){
+//
+export function set_posts(posts) {
     return {
         type: SET_POSTS,
         payload: posts
-    }
-} 
- export function create(params) {
-     console.log(JSON.stringify(params));
-    return (dispatch, getState) => {
-        axios.post('/makeApost', params)
-            
-          .then(function(response) {
-            dispatch(post_created())
-            console.log(response);
-          })
-          .catch(function (error) {
-            dispatch(post_failed())
-            console.log(error);
-          });
     };
- }
+}
+export function create(title, body, imagePath) {
+    return (dispatch, getState) => {
+        axios
+            .post('/makeApost', { title, body, imagePath })
 
-    export function createAccount(username, password) {  //remove params if second approach
-        
-        return(dispatch, getState ) =>{
-            // const state = getState();
-            // const params = {
-            // username: state.posts.username,
-            // password: state.posts.password
-            // }
-       return hashCreator(password).then((password)=>{
-            console.log(password);
-            
-        return axios.post('/createAccount', {username, password})
+            .then(function(response) {
+                dispatch(post_created());
+                console.log(response);
+            })
+            .catch(function(error) {
+                dispatch(post_failed());
+                console.log(error);
+            });
+    };
+}
 
-        
-        }).then(function(response){
-            dispatch(account_created());
-        }).catch(function(error) {
-            dispatch(account_failed());})
-            
-        }
-    }
+export function createAccount(username, password) {
+    //remove params if second approach
+
+    return (dispatch, getState) => {
+        // const state = getState();
+        // const params = {
+        // username: state.posts.username,
+        // password: state.posts.password
+        // }
+        return hashCreator(password)
+            .then(password => {
+                console.log(password);
+
+                return axios.post('/createAccount', { username, password });
+            })
+            .then(function(response) {
+                dispatch(account_created());
+            })
+            .catch(function(error) {
+                dispatch(account_failed());
+            });
+    };
+}
 
 export function logMeIn(username, password) {
     console.log(password);
     return (dispatch, getState) => {
-      return hashCreator(password).then((password)=>{
-          return  axios.post('/login', {username, password})         
-        }).then((response)=>{
-            dispatch(login_success());
-
-        }).catch((error)=>{
-            dispatch(login_failed());
-        })
-    }
+        return hashCreator(password)
+            .then(password => {
+                return axios.post('/login', { username, password });
+            })
+            .then(response => {
+                dispatch(login_success());
+            })
+            .catch(error => {
+                dispatch(login_failed());
+            });
+    };
 }
 
- function hashCreator(passwordPromise) {
-     console.log(process.env);
-        let password = bcrypt.hash(passwordPromise, process.env.REACT_APP_SECRET_CODE);
-        console.log(password);
-        return password
- }
- 
+function hashCreator(passwordPromise) {
+    console.log(process.env);
+    let password = bcrypt.hash(
+        passwordPromise,
+        process.env.REACT_APP_SECRET_CODE
+    );
+    console.log(password);
+    return password;
+}
 
- export function getPosts(){
+export function getPosts() {
     return (dispatch, getState) => {
-        axios.get('/getAllPosts').then((res)=>{
+        axios.get('/getAllPosts').then(res => {
             dispatch(set_posts(res.data));
-         });
-    }
-   
- }
+        });
+    };
+}
 
- export function editPost(params){
-     return (dispatch, getState) => {
-        return axios.put('/editApost', params).then((res)=>{
-           dispatch(getPosts());  
-         })
-     }
- }
+export function editPost(id, title, body, imagePath) {
+    return (dispatch, getState) => {
+        return axios
+            .put('/editApost', { _id: id, title, body, imagePath })
+            .then(res => {
+                dispatch(getPosts());
+            });
+    };
+}
 
- export function deletePost(params) {
-     return (dispatch, getState) => {
-         return axios.delete(`/post/${params}`).then((res)=>{
+export function deletePost(params) {
+    return (dispatch, getState) => {
+        return axios.delete(`/post/${params}`).then(res => {
             dispatch(getPosts());
-         });
-     }
- }
-
-  
+        });
+    };
+}
