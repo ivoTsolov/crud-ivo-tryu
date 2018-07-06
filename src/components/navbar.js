1d3ce1074b16926f135534b7a8f7a10389011ca0
alpox <elias.be@gmx.ch>
2018-07-06 22:10:23 +0200

    Change folder structure

---
+++ src/components/navbar.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown
+} from 'reactstrap';
+import { NavLink as Link } from 'react-router-dom';
+export default class Example extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">NEVERGRIM</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink tag={Link} to="/">
+                                Home
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/blog">
+                                Blog
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/about">
+                                About
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/register">
+                                Registrer
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/login">
+                                Login
+                            </NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar />
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        );
+    }
+}
